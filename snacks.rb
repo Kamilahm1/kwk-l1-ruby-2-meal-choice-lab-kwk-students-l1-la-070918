0d1343e97c40157ca3e995be28aa220bb7db7876
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-10 18:18:39 +0000

    automatically backed up by learn

--- snacks.rb
+++ snacks.rb
@@ -1,14 +1,14 @@
-def breakfast(food="Frosted Flakes")
+def breakfast(food="frosted flakes")
 puts "Morning is the best time for #{food}!"
 end 
 
-breakfast ("Frosted Flakes")
+breakfast ("frosted flakes")
 
-def lunch(food="Grilled Cheese")
+def lunch(food="grilled cheese")
   puts "Lunch is the best time for #{food}!"
 end
 
-lunch ("Grilled Cheese")
+lunch ("grilled cheese")
 
 def dinner(food="salmon")
 puts "Dinner is the best time for #{food}!"
