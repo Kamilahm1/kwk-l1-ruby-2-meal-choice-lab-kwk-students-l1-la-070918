90bc13e603412434643700b9de1df8616aa57fb1
Kamilahm1 <kamilahmorgan1@gmail.com>
2018-07-10 18:13:49 +0000

    automatically backed up by learn

--- snacks.rb
+++ snacks.rb
@@ -3,3 +3,9 @@ puts "Morning is the best time for #{food}!"
 end 
 
 breakfast ("Frosted Flakes")
+
+def lunch(food="Grilled Cheese")
+  puts "Lunch is the best time for #{food}!"
+end
+
+lunch ("Grilled Cheese")
